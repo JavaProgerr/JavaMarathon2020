ebf6a82a0f7939026b5614bf3d8602549f2cdc97
JavaProgerr <ligtor2010@gmail.com>
2020-10-28 12:44:41 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        boolean a = true;
+        while (a) {
+            double i = sc.nextDouble();
+            double j = sc.nextDouble();
+            if (j == 0) {
+                break;
+            }
+            System.out.println(i / j);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int b = 0;
+        while (b < 5) {
+            b++;
+            double i = sc.nextDouble();
+            double j = sc.nextDouble();
+            if (j == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(i / j);
+        }
     }
 }
