01ecbf28564b3a8cda4347cc43f8c37cf743bf06
JavaProgerr <ligtor2010@gmail.com>
2020-10-28 19:39:48 +0300

    complete day3 task 2

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean a = true;
-        while (a) {
+        while (true) {
             double i = sc.nextDouble();
             double j = sc.nextDouble();
             if (j == 0) {
