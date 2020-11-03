56aed2667a79f8bc471b327c052b5c261cc54888
JavaProgerr <ligtor2010@gmail.com>
2020-11-03 21:16:41 +0300

    complete day8 task

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String str = "";
 
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            str = str + i + " ";
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println("Время выполнения с String: "+ (finish-start));
+        System.out.println(str);
+
+        StringBuilder str2 = new StringBuilder();
+        start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            str2.append(i).append(" ");
+        }
+        finish = System.currentTimeMillis();
+        System.out.println("Время выполнения с StringBuilder: "+ (finish-start));
+        System.out.println(str2);
     }
 }
